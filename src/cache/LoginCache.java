ca9bfda8a46aaf5a9d4051731a78fc368eab2be9
Dustyzhao <820328544@qq.com>
2018-09-07 19:29:31 +0800

    user information storage

---
+++ src/cache/LoginCache.java
@@ -0,0 +1,43 @@
+package cache;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author DUSTY
+ */
+public class LoginCache {
+
+    private static LoginCache instance = new LoginCache();
+
+    //通过用户名获取sessionId，通过sessionId获取session。
+    private Map<String,String> loginUserSession = new HashMap<>();
+    private Map<String,HttpSession> loginSession = new HashMap<>();
+
+
+    private LoginCache() {
+
+    }
+
+    private static LoginCache getInstance() {
+        return instance;
+    }
+
+    public String getSessionIdByUsername(String username){
+        return loginUserSession.get(username);
+    }
+
+    public HttpSession getSessionBySessionId(String sessionId) {
+        return loginSession.get(sessionId);
+    }
+
+    public void setSessionIdByUsername(String username,String sessionId){
+        loginUserSession.put(username,sessionId);
+    }
+
+    public void setSessionBySessionId(String sessionId,HttpSession session){
+        loginSession.put(sessionId,session);
+    }
+
+}
