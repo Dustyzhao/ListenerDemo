bfa7b5d2451a868aee25f67292f9960d72e4c4fc
Dustyzhao <820328544@qq.com>
2018-09-07 22:57:15 +0800

    add code commentary

--- src/cache/LoginCache.java
+++ src/cache/LoginCache.java
@@ -5,13 +5,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * 登录用户与session缓存对象
  * @author DUSTY
  */
 public class LoginCache {
 
+    //先单例化
     private static LoginCache instance = new LoginCache();
 
-    //通过用户名获取sessionId，通过sessionId获取session。
+    /**
+     * httpSession对象创建后都会有一个唯一的ID用于与其他的session做标志区别。
+     * 但因为3.0的servlet api中已经不允许直接通过sessionId来获得session对象，
+     * 我们依然是通过sessionId来获取session对象，但现在只好间接地通过两步来获取，
+     * 我们把用户名和sessionID联系起来，即：1.通过用户名获取sessionId，2.通过sessionId获取session。
+     */
     private Map<String,String> loginUserSession = new HashMap<>();
     private Map<String,HttpSession> loginSession = new HashMap<>();
 
@@ -24,18 +31,38 @@ public class LoginCache {
         return instance;
     }
 
+    /**
+     * 通过登录名获取对应登录用户的sessionId
+     * @param username
+     * @return
+     */
     public String getSessionIdByUsername(String username){
         return loginUserSession.get(username);
     }
 
+    /**
+     * 通过sessionId获取对应的session对象
+     * @param sessionId
+     * @return
+     */
     public HttpSession getSessionBySessionId(String sessionId) {
         return loginSession.get(sessionId);
     }
 
+    /**
+     * 存储登录名与对应的登录sessionID至缓存对象
+     * @param username
+     * @param sessionId
+     */
     public void setSessionIdByUsername(String username,String sessionId){
         loginUserSession.put(username,sessionId);
     }
 
+    /**
+     * 存储sessionId与对应的session对象至缓存对象
+     * @param sessionId
+     * @param session
+     */
     public void setSessionBySessionId(String sessionId,HttpSession session){
         loginSession.put(sessionId,session);
     }
--- src/listener/LoginSessionListener.java
+++ src/listener/LoginSessionListener.java
@@ -16,24 +16,30 @@ public class LoginSessionListener implements HttpSessionAttributeListener {
 
     @Override
     public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
+        //监听到session属性值发生添加操作，获取对应操作的属性名
         String attrName = httpSessionBindingEvent.getName();
 
+        //若属性名为登录属性名，判定为用户登录操作
         if (LOGIN_USER.equals(attrName)) {
-            //获取当前登录名attrVal
+            //获取添加的属性值，即用户登录名
             String attrVal = (String) httpSessionBindingEvent.getValue();
+            //该次操作的session对象
             HttpSession session = httpSessionBindingEvent.getSession();
+            //该次操作的session对象ID
             String sessionId = session.getId();
 
-            //通过登录名获取缓存中的sessionId
+            //从缓存对象里面，获得该用户登录名对应的sessionID值
             String sessionId2 = LoginCache.getInstance().getSessionIdByUsername(attrName);
             if (null == sessionId2) {
-
-
+                //未获得结果，不需要清理前次登录用户会话信息
             }else {
+                //获取前次该用户登录对应的session对象
                 HttpSession session2 = LoginCache.getInstance().getSessionBySessionId(sessionId2);
+                //清理前次登录用户会话存储信息，使得前次登录失效
                 session2.invalidate();
             }
 
+            //完成该次登录用户登录名、sessionID，session对象的缓存对象存储
             LoginCache.getInstance().setSessionIdByUsername(attrVal,sessionId);
             LoginCache.getInstance().setSessionBySessionId(sessionId,session);
         }
